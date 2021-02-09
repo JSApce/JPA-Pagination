682c569045367fc98ad5b1400f50b3bf08ffa459
JSApce <jamerson.developer@gmail.com>
2021-02-09 16:35:31 -0300

    primeiro commit

---
+++ src/main/java/com/jsapce/jpapagination/JpapaginationApplication.java
@@ -0,0 +1,13 @@
+package com.jsapce.jpapagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpapaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpapaginationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsapce/jpapagination/JpapaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsapce.jpapagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpapaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
